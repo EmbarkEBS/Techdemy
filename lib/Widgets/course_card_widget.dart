d30c7ed52436b883828a9077c5549dcd77c80249
Karthikn-n <karthikn11092001@gmail.com>
2025-08-22 15:43:48 +0530

    Test revoked

--- lib/Widgets/course_card_widget.dart
+++ lib/Widgets/course_card_widget.dart
@@ -27,7 +27,7 @@ class CourseCardWidget extends StatelessWidget {
             children: [
               const SizedBox(height: 10,),
               SizedBox(
-                height: 50,
+                height: 100,
                 child: GetBuilder<CourseController>(
                   id: "courseDetail",
                   builder: (ctr) {
